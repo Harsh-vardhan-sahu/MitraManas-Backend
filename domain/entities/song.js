5dc0baaa548f8838cd3500f2ae8b85166e8da992
HarshVardhan Sahu <harshvardhansahoo7115@gmail.com>
2025-07-13 17:32:50 +0530

    fix: re-track Song.js after casing fix

---
+++ domain/entities/song.js
@@ -0,0 +1,11 @@
+class Song{
+    constructor({id,title,artist,link,image}){
+    this.id=id;
+    this.title=title;
+    this.artist=artist;
+    this.link=link;
+    this.image=image;
+    }
+}
+
+module.exports=Song;
